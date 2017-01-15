61ebfa87b2ce3cffbb42fc8e9ae920dea257506c
Nick Kantor <nick.kantor@utexas.edu>
2017-01-14 21:13:55 -0600

    Created a static version of the decision server

--- wheel.js
+++ wheel.js
@@ -26,3 +26,16 @@ Wheel.prototype.processData = function(csv) {
   }
   add_to_wheel(lines);
 }
+
+$( document ).ready(function() {
+  var wheel = new Wheel();
+  $(".file-upload-input").change(function(event) {
+    var file = event.currentTarget.files[0];
+    var r = new FileReader();
+    r.onload = function(e) {
+      var contents = e.target.result;
+      wheel.processData(contents);
+    }
+    r.readAsText(file);
+  });
+});
